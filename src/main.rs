a17def841da92deeb12b0a59f103be1a53a2cf33
zhangzilong <zzl@redmatrix.org.cn>
2022-07-06 16:27:03 +0800

    call shared library

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 
-extern crate libexample;
+
+
+use structopt::StructOpt;
+use std::path::PathBuf;
+use libexample::example::test;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "loading shared library")]
+struct Opt {
+    #[structopt(short, long, parse(from_os_str))]
+    shared: PathBuf,
+}
 
 fn main() {
     unsafe {
         libexample::example::test();
+        test();
         // example::test();
     }
+    let opt = Opt::from_args();
+
+        unsafe {
+            let lib = libloading::Library::new(
+              opt.shared
+            ).unwrap();
+            
+            let func: libloading::Symbol<unsafe extern "C" fn() -> i32  > =
+              lib.get(b"test").unwrap();
+            func();
+          }
+    
     println!("Hello, world!");
 }
